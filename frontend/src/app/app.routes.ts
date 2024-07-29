14c098fada9327fe06844d62a97c1e8bc03b85a5
alex <alex6205@yahoo.com>
2024-07-29 13:24:57 -0400

    NgModule

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,10 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [{ path: 'customers', component: CustomerListComponent }];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
